175bc481a6184634fd5c09537e0b6740231c87dc
henrypgill <henrygill3@gmail.com>
2023-08-22 17:55:23 +0100

    initial commit with structure from template

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: "./src/testSetup/setup.ts",
+        setupFiles: "./src/testing/setup.ts",
         // you might want to disable it, if you don't have tests that rely on CSS
         // since parsing CSS is slow
         css: true,
